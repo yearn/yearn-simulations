3effb95467edaa24f65587dd3df9dcf67202757d
retro-wavey <retrowaveyy@gmail.com>
2021-07-16 00:15:13 -0400

    added mm plugin

---
+++ plugins/s_0x036099f243E135cf2ADF449C2dD5497Da20FB82D.py
@@ -0,0 +1,16 @@
+import brownie
+from utils import dotdict
+from brownie import interface, accounts, web3, chain
+
+def pre_harvest_custom(data):
+    # Harvest underlying mm vault strategy
+    mm_gov = accounts.at("0x7cDaCBa026DDdAa0bD77E63474425f630DDf4A0D", force=True)
+    mmStrat = "0xa6f43d225d188AeF31F99F20eBa8E537a6DE86B5"
+    interface.IMMStrat(mmStrat).harvest({'from':mm_gov})
+    return data
+
+def post_harvest_custom(data):
+    return data
+
+def report_custom(data):
+    return data
