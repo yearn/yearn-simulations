c74bf18e15898a1733a4f7e95441b53bfd71e466
wavey <retrowaveyy@gmail.com>
2021-11-15 15:54:56 -0500

    feat: ftm support

--- bot/bot_poller.py
+++ bot/bot_poller.py
@@ -17,10 +17,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 
-
-# Web3 setup
-infura_id = os.environ.get("INFURA_ID")
-web3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/"+infura_id))
 with open("bot/abis/vault.json") as json_file:
     vault_abi = json.load(json_file)
 with open("bot/abis/strategy.json") as json_file:
@@ -37,22 +33,25 @@ def start(update, context):
     context.bot.send_message(chat_id=telegram_chat_id, text="I'm a bot, please talk to me!")
 
 @run_async
-def do_exec(address, chat_id):
+def do_exec(address, chat_id, chain_id):
     try:
         address = "-a "+ str(address)
         chat_id = "-i "+ str(chat_id)
+        chain_id = "-c "+ str(chain_id)
         print(chat_id)
-        p = subprocess.call(['bash','./bot/launch_simulator.sh', address, chat_id]) # Invoke shell script to update env vars and run brownie
+        p = subprocess.call(['bash','./bot/launch_simulator.sh', address, chat_id, chain_id]) # Invoke shell script to update env vars and run brownie
     except:
         e = sys.exc_info()
         print("error calling subprocess")
         print(e)
 
 
-def sim(update: Update, context: CallbackContext):
+def sim(update: Update, context: CallbackContext, chain_id, web3):
     name = ""
     isValid = False
     isVault = False
+    print("CHAIN ID", chain_id)
+    # Web3 setup
     str = ""
     print("CHAT ID")
     print(update.message)
@@ -66,7 +65,7 @@ def sim(update: Update, context: CallbackContext):
     try:
         address = context.args[0]
         if address == "all":
-            do_exec(address, chat_id) # Invoke shell script to update env vars and run brownie
+            do_exec(address, chat_id, chain_id) # Invoke shell script to update env vars and run brownie
             update.message.reply_text("G'day {}, ser!\n\nI see you'd like to simulate harvests on all strats\n\n💻 Let's run a simulation! ......".format(user_first_name,str))
         else:
             try:
@@ -94,7 +93,7 @@ def sim(update: Update, context: CallbackContext):
                         str = "vault:\n" + name
                     else:
                         str = "strategy:\n" + name
-                    do_exec(address, chat_id) # Invoke shell script to update env vars and run brownie
+                    do_exec(address, chat_id, chain_id) # Invoke shell script to update env vars and run brownie
                     update.message.reply_text("G'day {}, ser!\n\nAddress you gave is for {}\n\n💻 Let's run a harvest simulation! ......".format(user_first_name,str))
 
             except (IndexError, ValueError):
@@ -104,9 +103,21 @@ def sim(update: Update, context: CallbackContext):
     except (IndexError, ValueError):
         update.message.reply_text('Please supply a valid strategy or vault address.')
 
+def eth(update: Update, context: CallbackContext):
+    infura_id = os.environ.get("INFURA_ID")
+    web3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/"+infura_id))
+    sim(update, context, 1, web3)
+
+def ftm(update: Update, context: CallbackContext):
+    web3 = Web3(Web3.HTTPProvider("https://rpc.ftm.tools/"))
+    sim(update, context, 250, web3)
+
 start_handler = CommandHandler('start', start)
-sim_handler = CommandHandler('sim', sim)
+# sim_handler = CommandHandler('sim', sim)
+ftm_handler = CommandHandler('ftm', ftm)
+eth_handler = CommandHandler('eth', eth)
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(sim_handler)
+dispatcher.add_handler(ftm_handler)
+dispatcher.add_handler(eth_handler)
 
 updater.start_polling()
