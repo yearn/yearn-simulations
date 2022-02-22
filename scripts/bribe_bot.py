45cb74d802d46b629a1723d3fbfd9f8d3041e067
wavey0x <retrowaveyy@gmail.com>
2022-02-21 19:42:30 -0500

    feat: add debug logs

--- scripts/bribe_bot.py
+++ scripts/bribe_bot.py
@@ -2,7 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from datetime import datetime
-from ypricemagic.magic import get_price
+# from ypricemagic.magic import get_price
+from y import magic
+from y.contracts import logger
+magic.yearn.logger.addHandler(magic.yearn.logging.StreamHandler())
+magic.yearn.logger.setLevel(magic.yearn.logging.DEBUG)
 import telebot, requests
 from brownie import (
     Contract,
@@ -68,7 +72,7 @@ def main():
                 #     price = spell_price
                 # else:
                 #     price = oracle.getPriceUsdcRecommended(r) / 10**6
-                price = get_price(r)
+                price = magic.get_price(r)
                 token = Contract(r)
                 total_tokens = bribev2.reward_per_token(g, r) / 10**token.decimals()
                 total_tokens = total_tokens * gauge_controller.points_weight(g, period).dict()["slope"] / 1e18
