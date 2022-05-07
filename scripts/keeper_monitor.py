3d1144d803cb5d519fb09b39782d972294eaa4f3
wavey0x <retrowaveyy@gmail.com>
2022-05-06 22:23:21 -0400

    feat: fix error messages

--- scripts/keeper_monitor.py
+++ scripts/keeper_monitor.py
@@ -149,7 +149,7 @@ def critical_alert(code, info):
         symbol = CHAIN_VALUES[chain.id]["NETWORK_SYMBOL"]
         m += f'Keeper balance below {str(min_balance)} {symbol} threshold\n'
     if code == 1:
-        m = f'Exceeded {max_time_hrs} hrs threshold since last harvest.\n'
+        m = f'Exceeded {max_time_hrs} hrs threshold since last txn with unworked strategies.\n'
     unworked = info['UNWORKED']
     if len(unworked) > 0:
         m += f'\nUnworked strategies:\n'
