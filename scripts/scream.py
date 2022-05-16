6c5d1972501489b3c65ad538b90cac7b7dc16262
wavey0x <retrowaveyy@gmail.com>
2022-05-16 04:16:49 -0400

    feat: scream

--- scripts/scream.py
+++ scripts/scream.py
@@ -7,7 +7,7 @@ from brownie import (chain,web3, Contract, accounts,ZERO_ADDRESS)
 def main():
     print("Starting...", flush=True)
     load_dotenv(find_dotenv())
-    is_prod = os.environ.get("ENV") == "PROD"
+    is_prod = True #os.environ.get("ENV") == "PROD"
     telegram_bot_key = os.environ.get("WAVEY_ALERTS_BOT_KEY")
     chat_id = "-1001545486943"
     hot_account = accounts.load('scream', os.getenv('PASSWORD_SCREAM'))
@@ -18,21 +18,22 @@ def main():
 
     oracle = Contract("0x57AA88A0810dfe3f9b71a9b179Dd8bF5F956C46A")
 
-    # Markets
-    scDAI = Contract("0x8D9AED9882b4953a0c9fa920168fa1FDfA0eBE75")
-    scDOLA = Contract("0x5A3B9Dcdd462f264eC1bD56D618BF4552C2EaF8A")
-    scFRAX = Contract("0x383D965C8D2ac0A9c1F6930ad10943606BcA4cB7")
-
     markets = [
-        scDAI,
-        scDOLA,
-        scFRAX,
+        Contract("0x8D9AED9882b4953a0c9fa920168fa1FDfA0eBE75"), # scDAI
+        # Contract("0x5A3B9Dcdd462f264eC1bD56D618BF4552C2EaF8A"), # scDOLA
+        Contract("0x383D965C8D2ac0A9c1F6930ad10943606BcA4cB7"), # scFRAX
     ]
     
     strategies = [
-        Contract("0xd025b85db175EF1b175Af223BD37f330dB277786"),
-        Contract("0x36A1E9dF5EfdAB9694de5bFe25A9ACc23F66BCB7"),
-        Contract("0xfF8bb7261E4D51678cB403092Ae219bbEC52aa51"),
+        Contract("0xd025b85db175EF1b175Af223BD37f330dB277786"), # DAI
+        # Contract("0x36A1E9dF5EfdAB9694de5bFe25A9ACc23F66BCB7"), # DOLA
+        Contract("0xfF8bb7261E4D51678cB403092Ae219bbEC52aa51"), # FRAX
+    ]
+
+    prices = [
+        0,
+        0,
+        0,
     ]
 
     while True == True:
@@ -43,15 +44,18 @@ def main():
             strategy = strategies[i]
             message = ""
             count = count + 1
+            should_fetch_new_prices = count % 9_999 == 0
             should_fetch_new_prices = count % 10_000 == 0
 
             tx_params = {'from': hot_account}
-            tx_params['gas_price'] = 3_000e9
+            tx_params['gas_price'] = 1_000e9
 
             adjusted_balance = underlying.balanceOf(m) / 10**decimals
             val_in_strat = strategy.estimatedTotalAssets() / 10**decimals
+            price = prices[i]
             if price == 0 or should_fetch_new_prices:
-                price = oracle.getPriceUsdcRecommended(underlying) / 10**6
+                prices[i] = oracle.getPriceUsdcRecommended(underlying) / 10**6
+                price = prices[i]
                 print(f'fetched new {sym} price {"${:,.2f}".format(price)}', flush=True)
 
             usd_val = price * adjusted_balance # USD value of want tokens in the market
@@ -64,10 +68,18 @@ def main():
 
             # Send transaction if we are over threshold
             if usd_val >= usd_threshold and val_in_strat * price > usd_threshold:
+                # assert False
                 try:
                     tx = strategy.harvest(tx_params)
+                    txn_hash = tx.txid
                     debt_payment = tx.events["Harvested"]["debtPayment"]
-                    message = f'Transaction mined! \n{sym} {strategy.name()}\n\nDebt Payment: {debt_payment/10**decimals}'
+                    profit = tx.events["Harvested"]["profit"]
+                    retrieved = debt_payment + profit
+                    debtOutstanding = tx.events["Harvested"]["debtOutstanding"]
+                    url = "https://etherscan.io/tx/"
+                    if chain.id == 250:
+                        url = "https://ftmscan.com/tx/"
+                    message = f'⛏ Transaction mined!  --  {sym} {strategy.name()}\n\nRetrieved: {"${:,.2f}".format(retrieved/10**decimals)}\n\nRemaining debt in market: {"${:,.2f}".format(debtOutstanding / 10**decimals)}\n\n{url+txn_hash}'
                 except:
                     message = f'error sending a transaction.\n{sym} {strategy.name()}'
                 if is_prod:
